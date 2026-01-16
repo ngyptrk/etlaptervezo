f6b2e7eea18a7cb3fbe9f2a877a031646d506f30
ngy.ptrk <nagydpatrik@gmail.com>
2026-01-16 09:14:49 +0100

    Meal migracio

---
+++ server/app/Http/Controllers/MealController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Meal;
+use App\Http\Requests\StoreMealRequest;
+use App\Http\Requests\UpdateMealRequest;
+
+class MealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMealRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMealRequest $request, Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Meal $meal)
+    {
+        //
+    }
+}
---
+++ server/app/Models/Meal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meal extends Model
+{
+    /** @use HasFactory<\Database\Factories\MealFactory> */
+    use HasFactory;
+}
