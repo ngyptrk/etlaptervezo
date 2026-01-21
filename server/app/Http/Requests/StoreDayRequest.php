2adb1c59de3a6f7ebb78da54a8c5078b5c03fe8c
ngyptrk <nagydpatrik@gmail.com>
2026-01-21 10:36:56 +0100

    StoreRequestek kesz

--- server/app/Http/Requests/StoreDayRequest.php
+++ server/app/Http/Requests/StoreDayRequest.php
@@ -11,7 +11,7 @@ class StoreDayRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StoreDayRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'day' => ['required', 'string', 'max:255'],
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+            'meal_of_days_id' => ['required', 'integer', 'exists:meal_of_days,id'],
+            'recipe_id' => ['required', 'integer', 'exists:recipes,id'],
+            'meal_id' => ['required', 'integer', 'exists:meals,id'],
         ];
     }
 }
