277594024bb577c23195f5423047d88997fc929b
ngyptrk <nagydpatrik@gmail.com>
2026-01-21 09:18:10 +0100

    UserSeeder atiras

--- dokumentacio.md
+++ dokumentacio.md
@@ -6,6 +6,8 @@
 - Egyedi étlap/recept ajánlás napokra/hetekre.
 - A program képes választani az adatbázisban lévő receptekből és összeállítani napokra, vagy akár hetekre étrendet, melynek alapanyagait a bevásárláshoz leírja, és napra felosztja, hogy mit kell megvenni. Ezeket a recepteket lehet bővíteni saját receptekkel.
 
+
+
 ## Feladat felosztas:
 ### S. Zsolti:
 ### N. Patrik:
--- server/database/seeders/UserSeeder.php
+++ server/database/seeders/UserSeeder.php
@@ -21,14 +21,14 @@ class UserSeeder extends Seeder
             'role' => 1
         ]);
         User::factory()->create([
-            'name' => 'Raktáros',
-            'email' => 'raktaros@example.com',
+            'name' => 'Felhasználó',
+            'email' => 'felhasznalo@example.com',
             'password' => '123',
             'role' => 2
         ]);
         User::factory()->create([
-            'name' => 'Vásárló 1',
-            'email' => 'vasarlo1@example.com',
+            'name' => 'Látogató',
+            'email' => 'latogato@example.com',
             'password' => '123',
             'role' => 3
         ]);
